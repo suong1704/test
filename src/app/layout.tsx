8504e70b3beb60a1fd585ebc71f4b09512d1db90
Vmax <vmaxmartis@gmail.com>
2023-11-26 08:38:16 +0700

    add dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
   console.log(isLogin , 'issLogin');
   
  useEffect(() => {
- router.push(isLogin ? '/' : '/authentication/login')
+  if (!isLogin) {
+    router.push('/authentication/login')
+  }
  },[isLogin, pathname])
   
   return (
